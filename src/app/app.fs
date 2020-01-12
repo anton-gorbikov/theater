06d3b0f92bcc7ce2dbc73f56fd8a99187445bab4
Anton Gorbikov <anton.gorbikov@gmail.com>
2020-01-12 16:09:36 +0300

    feat: use implicit type cast

--- src/app/app.fs
+++ src/app/app.fs
@@ -1,7 +1,7 @@
 ﻿module App
 
-open Fable.Core
 open Fable.React
+open Fable.Core.JsInterop
 open ReactRouter.BrowserRouter
 open ReactRouter.Link
 open ReactRouter.Switch
@@ -10,7 +10,7 @@ open ReactRouter.Route
 let App() =
     BrowserRouter [] [
         div [] [str "Hello from Fable-React Application!"]
-        Link [To (U3.Case1 "/about")] [str "About"]
+        Link [To (!^ "/about")] [str "About"]
         Switch [] [
             Route [Path "/about"] [str "Content of the About page!"]
             Route [Path "/"] [str "Content of the Home page!"]
