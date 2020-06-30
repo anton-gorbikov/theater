487f1b27715d663cdf099a80ad1b514d166a4453
Anton Gorbikov <anton.gorbikov@gmail.com>
2020-06-30 23:18:54 +0000

    build: fix formatting

--- src/app/core/link.fs
+++ src/app/core/link.fs
@@ -5,15 +5,13 @@ open Fable.Core.JsInterop
 open Fable.React
 
 type ToObject =
-{
-    pathname: string
-    search: string
-    hash: string
-    state: string
-}
+    { pathname: string
+      search: string
+      hash: string
+      state: string }
 
 type LinkProps =
-    | To of U3<string, ToObject, (string -> string)>
+    | To of U3<string, ToObject, string -> string>
     | Replace of bool
 
 let inline Link (props: LinkProps list) (elems: ReactElement list): ReactElement =
