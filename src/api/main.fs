7ed6ba61adddee0fe467c787d0398d245cb20a79
Anton Gorbikov <anton.gorbikov@gmail.com>
2021-04-18 19:33:29 +0000

    deployment: add Heroku port usage

--- src/api/main.fs
+++ src/api/main.fs
@@ -1,9 +1,14 @@
 module Main
 
+open Fable.Core
+
 open express
 
 let application = Express.Create()
 
 application.get "/users" (fun _req res -> res.send "Users")
 
-application.listen 3000 (fun () -> printf "Theater API application started")
+[<Emit("process.env.port || 3000")>]
+let port : int = jsNative
+
+application.listen port (fun () -> printf "Theater API application started")
--- src/app/index.fs
+++ src/app/index.fs
@@ -20,4 +20,7 @@ let application = Express.Create()
 application.``use`` (Express.``static`` __dirname)
 application.``use`` ("/", (fun _ res _ -> (res.sendFile (__dirname + "/index.html"))))
 
-application.listen 4200 (fun () -> printf "Theater web application started")
+[<Emit("process.env.port || 4200")>]
+let port : int = jsNative
+
+application.listen port (fun () -> printf "Theater web application started")
